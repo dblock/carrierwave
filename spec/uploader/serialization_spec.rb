c9effc5a36eb46d7357e584fc8692e9f25ec33e4
dblock <dblock@dblock.org>
2013-02-28 17:02:14 -0500

    Repro for #10001: fails with undefined class/module SerializationSpec_Avatar::Uploader*.

---
+++ spec/uploader/serialization_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe CarrierWave::Uploader do
+
+  before :all do
+
+    class SerializationSpec_Avatar_Uploader < CarrierWave::Uploader::Base
+    end
+
+    class SerializationSpec_Avatar
+      extend CarrierWave::Mount
+      attr_accessor :name
+      mount_uploader :picture do
+
+      end
+    end
+
+  end
+
+  let(:cache) do
+    ActiveSupport::Cache.lookup_store(:memory_store)
+  end
+
+  describe 'ActiveSupport cache' do
+    it "stores an uploader" do
+
+      cache.fetch "key" do
+        image = SerializationSpec_Avatar.new
+        image.name = "hal"
+        image.picture
+        image
+      end
+
+      uploader_const = "Uploader#{SerializationSpec_Avatar.uploaders[:picture].object_id}"
+      SerializationSpec_Avatar.send :remove_const, uploader_const
+
+      cached_instance = cache.fetch "key" do
+        # not reached
+      end
+
+      cached_instance.name.should == "hal"
+
+    end
+  end
+end
